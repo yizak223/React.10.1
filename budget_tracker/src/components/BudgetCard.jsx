3704254f590c9f16f6fb8f0bf5d535bf295db1e0
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-01-14 23:21:05 +0200

    commit

--- budget_tracker/src/components/BudgetCard.jsx
+++ budget_tracker/src/components/BudgetCard.jsx
@@ -1,10 +1,12 @@
+import '../pages/BudgetTracker.css'
+
 function BudgetCard(props) {
     return(
-        <div>
-            <h1>{props.budgetTracker.title}</h1>
-            <h2>{props.budgetTracker.category}</h2>
-            <h3>{props.budgetTracker.amount}</h3>
-            <h4>{props.budgetTracker.type}</h4>
+        <div className="budgetCard">
+            <h1>Title: {props.budgetTracker.title}</h1>
+            <h4>Category: {props.budgetTracker.category}</h4>
+            <h4>Amount: {props.budgetTracker.amount}</h4>
+            <h4>Type: {props.budgetTracker.type}</h4>
         </div>
     )
 }
--- budget_tracker/src/pages/BudgetTracker.jsx
+++ budget_tracker/src/pages/BudgetTracker.jsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { useState } from "react";
 import BudgetCard from "../components/BudgetCard";
+import '../pages/BudgetTracker.css'
 
 //{title:'gas',category:'fuel',amount:1000,type:'income'}
 function BudgetTracker() {
@@ -15,34 +17,37 @@ function BudgetTracker() {
     const submitBudgetTracker =(e)=>{
         e.preventDefault()
         console.log([...budgetTrackers]);
-        setbudgetTrackers([...budgetTrackers,{...budgetTracker}])
+            setbudgetTrackers([...budgetTrackers,{...budgetTracker}]) 
+        
     }
     return(
-       <div>
-        
+       <div className="container">
+        <h1>Your Budget</h1>
         <form onSubmit={submitBudgetTracker}>
-                    <input onChange={changeBudgetTracker} type="text" placeholder="what's title?" name="title"/>
-                    <label htmlFor="">Category: </label>
-                        <select  onChange={changeBudgetTracker}  name="category" id="">
+                    <input className="formInput" onChange={changeBudgetTracker} type="text" placeholder="what's title?" name="title"/>
+                    <label className="label" htmlFor="category">Category: </label>
+                        <select className="formInput" onChange={changeBudgetTracker}  name="category" id="">
                             <option value="mortgage">mortgage</option>
                             <option value="shopping">shopping</option>
                             <option value="fuel">fuel</option>
                             <option value="Loans">Loans</option>
                             <option value="salary">salary</option>
                         </select>
-                    <input placeholder="How much?" onChange={changeBudgetTracker} type="text"  name="amount"/>
-                    <label htmlFor="type">Type: </label>
-                        <select  onChange={changeBudgetTracker} name="type" id="">
+                    <input className="formInput" placeholder="How much?" onChange={changeBudgetTracker} type="text"  name="amount"/>
+                    <label className="label" htmlFor="type">Type: </label>
+                        <select className="formInput" onChange={changeBudgetTracker} name="type" id="">
                             <option value="income">income</option>
                             <option value="outcome">outcome</option>
                         </select>
-                    <button type="submit">Add Product</button>
+                    <button className="button" type="submit">Add Product</button>
                 </form>
+                <div className="budgetContainer">
             {budgetTrackers.map((budgetTracker,index)=>{
                 return (
                 <BudgetCard budgetTracker={budgetTracker} key={index}/>
                 )
             })}
+            </div>
        </div>
     )
 }
